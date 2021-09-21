65709c7c23d03cf922d1a3bda544b90f61085b95
kevith bastidas <kevithchi@unicauca.edu.co>
2021-09-21 17:21:31 -0500

    .

--- AppRestauranteOnline/micro-servicios/Servicio-Restaurante/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO restaurante (rest_id,rest_nombre,rest_direccion,rest_ciudad,rest_telefono) VALUES (1,'LA COSECHA','Cra 11 # 3-45','Popayan','800001');
-INSERT INTO restaurante (rest_id,rest_nombre,rest_direccion,rest_ciudad,rest_telefono) VALUES (2,'PATATUS','Cll 3a # 4-25 n','Popayan','800002');
-INSERT INTO restaurante (rest_id,rest_nombre,rest_direccion,rest_ciudad,rest_telefono) VALUES (3,'PIZZA PITS','cra 3s # 3-21','Popayan','800003');
-INSERT INTO restaurante (rest_id,rest_nombre,rest_direccion,rest_ciudad,rest_telefono) VALUES (4,'PIO PIO','cll 2 # 26-11','Popayan','800004');
